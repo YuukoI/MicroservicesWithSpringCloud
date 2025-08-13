5b3c99dc04ea5bef1be0f5bc47118180f8bad88b
Juampi <juampiigorosito@gmail.com>
2025-08-13 10:30:33 -0300

    Eureka Server

--- motorbike-service/motorbike-service/src/main/java/com/motorbike/service/MotorbikeServiceApplication.java
+++ motorbike-service/motorbike-service/src/main/java/com/motorbike/service/MotorbikeServiceApplication.java
@@ -2,8 +2,10 @@ package com.motorbike.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotorbikeServiceApplication {
 
 	public static void main(String[] args) {
