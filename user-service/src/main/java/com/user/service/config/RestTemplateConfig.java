91a733462595c8019725a78316f87d9232163734
Juampi <juampiigorosito@gmail.com>
2025-11-05 12:38:52 -0300

    Zipkin Brave

--- user-service/src/main/java/com/user/service/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/user/service/config/RestTemplateConfig.java
@@ -1,5 +1,11 @@
 package com.user.service.config;
 
+import brave.Tracing;
+import brave.http.HttpTracing;
+import brave.propagation.CurrentTraceContext;
+import brave.sampler.Sampler;
+import brave.spring.web.TracingClientHttpRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +15,24 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public Tracing tracing() {
+        return Tracing.newBuilder()
+                .localServiceName("user-service")
+                .sampler(Sampler.ALWAYS_SAMPLE)
+                .currentTraceContext(CurrentTraceContext.Default.create())
+                .build();
     }
 
+    @Bean
+    public HttpTracing httpTracing(Tracing tracing) {
+        return HttpTracing.create(tracing);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder, HttpTracing httpTracing) {
+        return builder
+                .additionalInterceptors(TracingClientHttpRequestInterceptor.create(httpTracing))
+                .build();
+    }
 }
