2704daf699b86ed5376d74f007ddba8acddd13cd
Juampi <juampiigorosito@gmail.com>
2025-11-05 14:49:53 -0300

    Fixes

---
+++ motorbike-service/src/main/java/com/motorbike/service/config/BraveConfig.java
@@ -0,0 +1,24 @@
+package com.motorbike.service.config;
+
+import brave.Tracing;
+import brave.http.HttpTracing;
+import brave.sampler.Sampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BraveConfig {
+
+    @Bean
+    public Tracing tracing() {
+        return Tracing.newBuilder()
+                .localServiceName("motorbike-service")
+                .sampler(Sampler.create(1.0f))
+                .build();
+    }
+
+    @Bean
+    public HttpTracing httpTracing(Tracing tracing) {
+        return HttpTracing.create(tracing);
+    }
+}
--- user-service/src/main/java/com/user/service/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/user/service/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.user.service.config;
 
+import brave.http.HttpTracing;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +14,4 @@ public class RestTemplateConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
 }
