9e5427f9cf6e640ee198456a34ffdcc5c00871ac
Juampi <juampiigorosito@gmail.com>
2025-11-05 14:04:56 -0300

    Fixes

--- user-service/src/main/java/com/user/service/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/user/service/config/RestTemplateConfig.java
@@ -1,11 +1,5 @@
 package com.user.service.config;
 
-import brave.Tracing;
-import brave.http.HttpTracing;
-import brave.propagation.CurrentTraceContext;
-import brave.sampler.Sampler;
-import brave.spring.web.TracingClientHttpRequestInterceptor;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,24 +9,9 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public Tracing tracing() {
-        return Tracing.newBuilder()
-                .localServiceName("user-service")
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .currentTraceContext(CurrentTraceContext.Default.create())
-                .build();
-    }
-
-    @Bean
-    public HttpTracing httpTracing(Tracing tracing) {
-        return HttpTracing.create(tracing);
-    }
-
-    @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(RestTemplateBuilder builder, HttpTracing httpTracing) {
-        return builder
-                .additionalInterceptors(TracingClientHttpRequestInterceptor.create(httpTracing))
-                .build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
+
 }
