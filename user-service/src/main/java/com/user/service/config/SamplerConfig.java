bc4cea31b0f494ad9f6401a34709c97dc19f0d84
Juampi <juampiigorosito@gmail.com>
2025-11-07 08:46:56 -0300

    Brave / Zipkin / Micrometer

---
+++ user-service/src/main/java/com/user/service/config/SamplerConfig.java
@@ -0,0 +1,14 @@
+package com.user.service.config;
+
+import brave.sampler.Sampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SamplerConfig {
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
+
